d4f84c148bd3cbda76ab1e75cddc42a12d7ba20d
Gabriel Hideki <ghideki21@gmail.com>
2026-05-26 18:17:29 -0300

    Atualizando estrutura do projeto

---
+++ backend/src/main/java/br/com/imobigest/ImobigestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.imobigest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobigestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobigestApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/imobigest/ImobigestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.imobigest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobigestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
